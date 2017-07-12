b082e9514670b16dc4f495d7813c4b40e54546e8
Maia Ross <Maia.Ross@jda.com>
2017-07-12 14:20:33 -0400

    colors randomly generate on key press

--- Bee/Main.cpp
+++ Bee/Main.cpp
@@ -1,10 +1,10 @@
 #include <SFML/Graphics.hpp>
-#include <array>
 #include <iostream>
 
 //header
 int main();
 sf::Color genRandomColor();
+void setUpTextures(sf::Texture textures[]);
 int setUpSprites(sf::Sprite sprites[], sf::Texture textures[]);
 void recolorSprites(sf::Sprite sprites[]);
 void recolorSprite(sf::Sprite sprite);
@@ -18,14 +18,17 @@ int main()
   sf::RenderWindow window(sf::VideoMode(800, 600), "Press Any Key To Generate New Palette");
 
   sf::Texture textures[palSize];
+
+  setUpTextures(textures);
+
   sf::Sprite sprites[palSize];
 
   setUpSprites(sprites, textures);
-
+  
 
   while (window.isOpen())
   {
-    
+
     sf::Event event;
     while (window.pollEvent(event))
     {
@@ -33,15 +36,19 @@ int main()
         window.close();
       if (event.type == sf::Event::KeyPressed)
       {
-        recolorSprites(sprites);
+        for (int i = 0; i <= palSize; i++)
+        {
+          sprites[i].setColor(genRandomColor());
+        }
       }
     }
     
+//recolorSprites(sprites);
+
     window.clear(sf::Color::Black);
 
-    for (int i = 0; i < palSize; i++)
-    {
-      
+    for (int i = 0; i < palSize; i++) {
+      window.draw(sprites[i]); 
     }
     
     window.display();
@@ -51,34 +58,37 @@ int main()
   return 0;
 }
 
+//helpers
+
 sf::Color genRandomColor()
 {
   auto r = rand() % 255;
   auto g = rand() % 255;
   auto b = rand() % 255;
+  std::cout << "New color: " << r << " " << g << " " << b << "\n";
   return sf::Color(r, g, b);
+}
+
+void setUpTextures(sf::Texture textures[])
+{
+  for (int i = 0; i < palSize; i++)
+  {
+    if (!textures[i].loadFromFile("..\\ant.png"))
+    {
+      //error
+    }
+  }
+  std::cout << "Textures setup complete.\n";
 }
 
 int setUpSprites(sf::Sprite sprites[], sf::Texture textures[])
 {
   for (int i = 0; i < palSize; i++)
   {
-    if (!textures[i].create(160, 600)) { return -1; }
     sprites[i].setTexture(textures[i]);
+    sprites[i].setScale(160, 600);
     sprites[i].setPosition(sf::Vector2f(i * 160, 0));
   }
+  std::cout << "Sprites setup complete.\n";
   return 0;
-}
-
-void recolorSprites(sf::Sprite sprites[])
-{
-  for (int i = 0; i < palSize; i++)
-  {
-    recolorSprite(sprites[i]);
-  }
-}
-
-void recolorSprite(sf::Sprite sprite)
-{
-  sprite.setColor(genRandomColor());
 }
